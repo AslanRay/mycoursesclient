b55fddd90683bc01e9c06cf77583a597803572a3
Raymundo <aslan_ray@icloud.com>
2020-11-09 09:21:34 -0800

    Started app structure

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [0],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ src/components/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginForm() {
+  return (
+    <div>
+      <span>LoginForm</span>
+    </div>
+  );
+}
+
+export default LoginForm;
